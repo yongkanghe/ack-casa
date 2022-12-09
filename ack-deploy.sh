7b7a3ecb73f7f4f91f2e8268ebb0d68aa75061f0
Yongkang He <he@yongkang.cloud>
2022-12-09 11:16:32 +0800

    Update ack-deploy.sh

--- ack-deploy.sh
+++ ack-deploy.sh
@@ -55,14 +55,14 @@ aliyun cs DescribeClusterUserKubeconfig --region $MY_REGION --ClusterId $(cat ac
 # Run below command if yamlify2 does not exist
 cat <<EOF > ~/.jq
 def yamlify2:
-    (objects | to_entries | (map(.key | length) | max + 2) as $w |
-        .[] | (.value | type) as $type |
-        if $type == "array" then
+    (objects | to_entries | (map(.key | length) | max + 2) as \$w |
+        .[] | (.value | type) as \$type |
+        if \$type == "array" then
             "\(.key):", (.value | yamlify2)
-        elif $type == "object" then
+        elif \$type == "object" then
             "\(.key):", "    \(.value | yamlify2)"
         else
-            "\(.key):\(" " * (.key | $w - length))\(.value)"
+            "\(.key):\(" " * (.key | \$w - length))\(.value)"
         end
     )
     // (arrays | select(length > 0)[] | [yamlify2] |
