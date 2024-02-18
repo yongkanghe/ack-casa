3169cf5a98e1072658bbcebf31aa45ea69920c3a
Yongkang He <he@yongkang.cloud>
2024-02-18 20:23:51 +1100

    Update pg-deploy

--- pg-deploy.sh
+++ pg-deploy.sh
@@ -1,6 +1,6 @@
 echo '-------Deploy a PostgreSQL sample database'
 
 kubectl create namespace yong-postgresql
-helm3 repo add bitnami https://charts.bitnami.com/bitnami
-helm3 repo update
-helm3 install --namespace yong-postgresql postgres bitnami/postgresql --set primary.persistence.size=20Gi
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install --namespace yong-postgresql postgres bitnami/postgresql --set primary.persistence.size=20Gi
